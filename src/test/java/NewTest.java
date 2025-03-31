f4368352fe7cb1115e0196f804df6a88d4ee7131
NikitaKarickii2000 <333crazy.man333@gmail.com>
2025-03-31 20:35:01 +0300

    комит

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,53 +1,86 @@
-import org.example.ArithmeticOperations;
-import org.example.Factorial;
-import org.example.NumberComparator;
-import org.example.TriangleArea;
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NewTest {
+    WebDriver driver;
+    WebDriverWait wait;
 
-        @Test
-    public void testNumberComparison() {
-        assertEquals(NumberComparator.compare(5, 3), "5 больше чем 3");
-        assertEquals(NumberComparator.compare(2, 4), "2 меньше чем 4");
-        assertEquals(NumberComparator.compare(7, 7), "7 равно 7");
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test
-    public void testArithmeticOperations() {
-        assertEquals(ArithmeticOperations.add(5, 3), 8);
-        assertEquals(ArithmeticOperations.subtract(5, 3), 2);
-        assertEquals(ArithmeticOperations.multiply(5, 3), 15);
-        assertEquals(ArithmeticOperations.divide(6, 3), 2.0);
-    }
+    public void verifyBlockTitle() {
+        driver.get("https://www.mts.by/");
+        WebElement block = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2"));
 
-    @Test(expectedExceptions = ArithmeticException.class,
-            expectedExceptionsMessageRegExp = "Деление на ноль")
-    public void testDivisionByZero() {
-        ArithmeticOperations.divide(5, 0);
-    }
+        String expectedTitle = "Онлайн пополнение\n" +
+                "без комиссии";
+        String actualTitle = block.getText();
 
+        assertEquals(expectedTitle, actualTitle, "Название блока не совпадает");
+    }
     @Test
-    public void testTriangleAreaCalculation() {
-        assertEquals(TriangleArea.calculateArea(5, 4), 10.0);
-    }
+    public void checkLogos() {
+        driver.get("https://www.mts.by/");
 
-    @Test(expectedExceptions = IllegalArgumentException.class,
-            expectedExceptionsMessageRegExp = "Значения должны быть положительные")
-    public void testInvalidTriangleParameters() {
-        TriangleArea.calculateArea(-2, 3);
-    }
+        List<WebElement> logos = wait.until(ExpectedConditions
+                .presenceOfAllElementsLocatedBy(
+                        By.cssSelector("#pay-section > div > div > div.col-12.col-xl-8 > section > div > div.pay__partners > ul"
+                        )));
 
+        Assertions.assertAll(
+                "Проверка логотипов",
+                () -> Assertions.assertFalse(logos.isEmpty(),
+                        "Не найдено ни одного логотипа"));
+    }
     @Test
-    public void testFactorialCalculation() {
-        assertEquals(Factorial.calculateFactorial(5), 120L);
-        assertEquals(Factorial.calculateFactorial(0), 1L);
+    public void checkDetailsLink() {
+        driver.get("https://www.mts.by/");
+        WebElement link = driver.findElement(By.partialLinkText("Подробнее о сервисе"));
+        link.click();
+        Assertions.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/",
+                                driver.getCurrentUrl());
+    }
+    @Test
+    public void testContinueButton() {
+        driver.get("https://www.mts.by/");
+        WebElement phoneInput = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(
+                        By.cssSelector("#connection-phone")
+                ));
+
+        phoneInput.sendKeys("297777777");
+
+        WebElement continueButton = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//*[@id=\"pay-connection\"]/button")
+                ));
+
+        Assertions.assertTrue(continueButton.isEnabled(),
+                "Кнопка должна быть активной");
     }
 
-    @Test(expectedExceptions = IllegalArgumentException.class,
-            expectedExceptionsMessageRegExp = "Отрицательные числа не допускаются")
-    public void testNegativeFactorial() {
-        Factorial.calculateFactorial(-1);
-    }
+
+        @AfterEach
+        public void teardown () {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
 }
